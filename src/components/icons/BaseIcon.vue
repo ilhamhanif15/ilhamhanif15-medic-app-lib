7fb5e9c770f2670692990ae0e4ff0623b05015fb
Hanif <ilhamhanif1512@gmail.com>
2022-03-15 17:04:59 +0700

    First Init

---
+++ src/components/icons/BaseIcon.vue
@@ -0,0 +1,40 @@
+<template>
+  <component
+    :is="iconComponent"
+    role="img"
+  />
+</template>
+
+<script>
+import ClockIcon from "./ClockIcon.vue"
+import CalendarIcon from "./CalendarIcon.vue"
+import ProfileIcon from "./ProfileIcon.vue"
+import HomeIcon from "./HomeIcon.vue"
+import CameraIcon from "./CameraIcon.vue"
+
+const icons = {
+  clock: ClockIcon,
+  calendar: CalendarIcon,
+  profile: ProfileIcon,
+  home: HomeIcon,
+  camera: CameraIcon,
+}
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+      validator(value) {
+        return Object.prototype.hasOwnProperty.call(icons, value)
+      }
+    }
+  },
+
+  computed: {
+    iconComponent() {
+      return icons[this.name]
+    }
+  }
+}
+</script>
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
+export { default as BaseIcon } from './icons/BaseIcon.vue';
+
+// Other components you plan to publish go here
