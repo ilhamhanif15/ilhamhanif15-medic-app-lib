edaa5af77af61615a35eb980f6bd24d6199a9630
Hanif <ilhamhanif1512@gmail.com>
2022-03-15 23:34:54 +0700

    Add to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,18 @@
 /* eslint-disable import/prefer-default-export */
 
 export { default as BaseIcon } from './icons/BaseIcon.vue';
+export { default as BaseButton } from './buttons/BaseButton.vue';
+
+export { default as ProfileSnippet } from './cards/ProfileSnippet.vue';
+export { default as QueueCard } from './cards/QueueCard.vue';
+export { default as TopBackground } from './cards/TopBackground.vue';
+export { default as UserProfileSnippet } from './cards/UserProfileSnippet.vue';
+
+export { default as BrowseImage } from './inputs/BrowseImage.vue';
+export { default as SelectInput } from './inputs/SelectInput.vue';
+export { default as TextAreaInput } from './inputs/TextAreaInput.vue';
+export { default as TextInput } from './inputs/TextInput.vue';
+export { default as PasswordInput } from './inputs/fragments/PasswordInput.vue';
+
 
 // Other components you plan to publish go here
